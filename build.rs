e324f3582d71469da2bab9c008b1c7f410067e29
Nolan Darilek <nolan@thewordnerd.info>
2019-02-07 20:44:18 -0600

    Initial stab at vendoring Spatialite and dependencies.

--- build.rs
+++ build.rs
@@ -1,20 +1,41 @@
-extern crate bindgen;
-extern crate pkg_config;
-
 use std::env;
 use std::path::Path;
 use std::process::Command;
 
+fn compile() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-lib=static=geos");
+    let dst = autotools::build("vendor/proj-5.2.0");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=proj");
+    let config = autotools::Config::new("vendor/geos-3.7.1")
+        .cflag(format!("-I {}/include", out_dir))
+        .ldflag(format!("-L{}/lib", out_dir))
+        .build();
+    println!("cargo:rustc-link-search=native={}", config.display());
+    println!("cargo:rustc-link-lib=static=geos");
+    let dst = autotools::build("vendor/freexl-1.0.5");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=freexl");
+    let config = autotools::Config::new("vendor/libspatialite-4.3.0a")
+        .cflag(format!("-I {}/include", out_dir))
+        .ldflag(format!("-L{}/lib", out_dir))
+        .with("-geosconfig", Some(&format!("{}/bin/geos-config", out_dir)))
+        .build();
+    println!("cargo:rustc-link-search=native={}", config.display());
+    println!("cargo:rustc-link-lib=static=spatialite");
+}
+
 fn main() {
     let has_pkgconfig = Command::new("pkg-config").output().is_ok();
     if has_pkgconfig {
-        if pkg_config::find_library("libgit2").is_ok() {
+        if pkg_config::find_library("spatialite").is_ok() {
             pkg_config::probe_library("spatialite").unwrap();
         } else {
-            println!("cargo:rustc-link-lib=spatialite");
+            compile();
         }
     } else {
-        println!("cargo:rustc-link-lib=spatialite");
+        compile();
     }
     let out_dir = env::var("OUT_DIR").unwrap();
     let _ = bindgen::builder()
