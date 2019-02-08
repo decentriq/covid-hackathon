9830b05b29e8328ffedb2da933ef2ddf1f621ef2
Nolan Darilek <nolan@thewordnerd.info>
2019-02-08 13:05:37 -0600

    Try linking against C++ libraries.

--- build.rs
+++ build.rs
@@ -30,6 +30,14 @@ fn compile() {
         .build();
     println!("cargo:rustc-link-search=native={}/lib", config.display());
     println!("cargo:rustc-link-lib=static=spatialite");
+    let target  = env::var("TARGET").unwrap();
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    } else if target.contains("linux") {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    } else {
+        unimplemented!();
+    }
 }
 
 fn main() {
