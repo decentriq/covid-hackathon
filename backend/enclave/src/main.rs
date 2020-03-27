f0569b97af35b58f517c25feb3619374ab9173a0
exfalso <0slemi0@gmail.com>
2020-03-27 16:05:48 +0100

    in-memory logic

--- backend/enclave/src/main.rs
+++ backend/enclave/src/main.rs
@@ -5,7 +5,7 @@ use hyper::server::Response;
 use rusqlite::params;
 use rusqlite::Connection;
 use std::io::Read;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use hyper::uri::RequestUri::AbsolutePath;
 use hyper::net::Fresh;
 use std::sync::Arc;
@@ -14,19 +14,44 @@ use serde::Deserialize;
 use serde::Serialize;
 use chrono::DateTime;
 use chrono::Utc;
+use time::Duration;
+use std::ops::Sub;
+use std::borrow::{BorrowMut, Borrow};
+use log::info;
+use log::error;
 
 type UserId = String;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+struct Configuration {
+    // The time distance under which a point is considered exposed wrt an infected point.
+    exposure_time_distance: Duration,
+    // The space distance under which a point is considered exposed wrt an infected point.
+    exposure_space_distance: f32,
+    // The interval of the enclave scanning and updating its state.
+    update_interval: Duration,
+}
+
 struct EnclaveState {
     connection: Connection,
-    to_notify: HashSet<UserId>,
+    exposed: HashMap<UserId, DateTime<Utc>>,
+    last_update: Option<DateTime<Utc>>,
+
+    temp_in_memory: Vec<TempEntry>,
+}
+
+struct TempEntry {
+    user_id: UserId,
+    infected: bool,
+    timestamped_coordinate: TimestampedCoordinate,
 }
 
 struct EnclaveHandler {
     state: Arc<Mutex<EnclaveState>>,
+    configuration: Configuration,
 }
 
+#[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 struct TimestampedCoordinate {
     timestamp: DateTime<Utc>,
@@ -34,30 +59,116 @@ struct TimestampedCoordinate {
     y: f32,
 }
 
+#[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 struct PollRequest {
     user_id: UserId,
+    infected: bool,
     timestamped_coordinates: Vec<TimestampedCoordinate>,
 }
 
+#[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 struct PollResponse {
-
+    exposed_timestamp: Option<DateTime<Utc>>,
 }
 
 impl EnclaveHandler {
     fn new(connection: Connection) -> EnclaveHandler {
         let state = EnclaveState {
             connection,
-            to_notify: HashSet::new(),
+            exposed: HashMap::new(),
+            last_update: None,
+            temp_in_memory: vec![],
         };
-
-        EnclaveHandler { state: Arc::new(Mutex::new(state)) }
+        let utc: DateTime<Utc> = Utc::now();
+        let asd: Duration = utc.sub(utc);
+        EnclaveHandler {
+            state: Arc::new(Mutex::new(state)),
+            configuration: Configuration::default(),
+        }
     }
 
     fn handle_poll(&self, req: &mut Request) -> Result<Vec<u8>> {
-        let poll_request: PollRequest = serde_json::from_reader(req)?;
-        Ok(serde_json::to_vec(&poll_request).unwrap())
+        let mut poll_request: PollRequest = serde_json::from_reader(req)?;
+        let poll_response = {
+            let mut guard = self.state.lock().unwrap();
+            EnclaveHandler::insert_new_points(guard.borrow_mut(), &mut poll_request);
+            EnclaveHandler::update_if_necessary(guard.borrow_mut(), &poll_request, &self.configuration);
+            EnclaveHandler::query_exposed(guard.borrow(), &poll_request)
+        };
+        info!("Poll response {:?}", poll_response);
+        Ok(serde_json::to_vec(&poll_response).unwrap())
+    }
+
+    fn update_if_necessary(state: &mut EnclaveState, request: &PollRequest, configuration: &Configuration) {
+        let latest_timestamped_coordinate = request.timestamped_coordinates.iter().max_by(|a, b| {
+            a.timestamp.cmp(&b.timestamp)
+        });
+        let latest_timestamp = match latest_timestamped_coordinate {
+            None => {
+                // Don't update if there are no timestamps in the request at all
+                return
+            },
+            Some(coord) => {
+                // If there was at least one update already..
+                if let Some(last_update) = state.last_update {
+                    // .. but not enough time has passed ..
+                    if coord.timestamp - last_update < configuration.update_interval {
+                        // .. then don't update ..
+                        return
+                    }
+                }
+                // Otherwise update
+                coord.timestamp
+            }
+        };
+
+        info!("Updating database");
+
+        let delta_squared = configuration.exposure_space_distance * configuration.exposure_space_distance;
+        for entry in &state.temp_in_memory {
+            if let Some(exposure_time) = state.exposed.get(&entry.user_id) {
+                // If we know the user is marked exposed with a more recent timestamp, don't scan.
+                if exposure_time > &entry.timestamped_coordinate.timestamp {
+                    continue
+                }
+            }
+            let coord = &entry.timestamped_coordinate;
+            for other_entry in &state.temp_in_memory {
+                if other_entry.user_id == entry.user_id {
+                    continue
+                }
+                let dx = entry.timestamped_coordinate.x - other_entry.timestamped_coordinate.x;
+                let dy = entry.timestamped_coordinate.y - other_entry.timestamped_coordinate.y;
+                let dtime = entry.timestamped_coordinate.timestamp - other_entry.timestamped_coordinate.timestamp;
+                if dtime < configuration.exposure_time_distance && dx * dx + dy * dy < delta_squared {
+                    state.exposed.insert(entry.user_id.clone(), entry.timestamped_coordinate.timestamp);
+                    break
+                }
+            }
+        }
+
+        state.last_update = Some(latest_timestamp);
+    }
+
+    fn insert_new_points(state: &mut EnclaveState, request: &mut PollRequest) {
+        info!("Inserting new points of {}", request.user_id);
+
+        for timestamped_coordinate in request.timestamped_coordinates.drain(0..) {
+            state.temp_in_memory.push(TempEntry {
+                user_id: request.user_id.clone(),
+                infected: request.infected,
+                timestamped_coordinate
+            });
+        }
+    }
+
+    fn query_exposed(state: &EnclaveState, request: &PollRequest) -> PollResponse {
+        info!("Querying exposed status of {}", request.user_id);
+        PollResponse {
+            exposed_timestamp: state.exposed.get(&request.user_id).cloned()
+        }
     }
 }
 
@@ -71,6 +182,7 @@ impl hyper::server::Handler for EnclaveHandler {
                             res.send(response_contents.as_slice()).unwrap();
                         }
                         Err(err) => {
+                            error!("Error during request processing: {:?}", err);
                             *res.status_mut() = hyper::BadRequest;
                             res.send(format!("{:?}", err).as_bytes()).unwrap();
                         }
@@ -89,6 +201,16 @@ impl hyper::server::Handler for EnclaveHandler {
     }
 }
 
+impl Default for Configuration {
+    fn default() -> Self {
+        Configuration {
+            exposure_time_distance: Duration::seconds(30),
+            exposure_space_distance: 3.0,
+            update_interval: Duration::seconds(1),
+        }
+    }
+}
+
 fn main() {
     setup_logging();
     let connection = Connection::open_in_memory().unwrap();
@@ -98,7 +220,7 @@ fn main() {
 }
 
 fn setup_logging() {
-    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_LOG", "info");
     env_logger::Builder::from_default_env()
         .target(env_logger::Target::Stdout)
         .init();
