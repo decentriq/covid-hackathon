30434f3856bbcdaca3747473a11d57a836dbd0c5
exfalso <0slemi0@gmail.com>
2020-03-27 12:44:24 +0100

    serde/json/chrono stuff

--- backend/enclave/src/main.rs
+++ backend/enclave/src/main.rs
@@ -4,14 +4,19 @@ use hyper::server::Request;
 use hyper::server::Response;
 use rusqlite::params;
 use rusqlite::Connection;
-use rusqlite::Result;
 use std::io::Read;
 use std::collections::HashSet;
 use hyper::uri::RequestUri::AbsolutePath;
 use hyper::net::Fresh;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use std::sync::Mutex;
+use serde::Deserialize;
+use serde::Serialize;
+use chrono::DateTime;
+use chrono::Utc;
 
 type UserId = String;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 struct EnclaveState {
     connection: Connection,
@@ -20,6 +25,24 @@ struct EnclaveState {
 
 struct EnclaveHandler {
     state: Arc<Mutex<EnclaveState>>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TimestampedCoordinate {
+    timestamp: DateTime<Utc>,
+    x: f32,
+    y: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PollRequest {
+    user_id: UserId,
+    timestamped_coordinates: Vec<TimestampedCoordinate>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PollResponse {
+
 }
 
 impl EnclaveHandler {
@@ -32,9 +55,9 @@ impl EnclaveHandler {
         EnclaveHandler { state: Arc::new(Mutex::new(state)) }
     }
 
-
-    fn handle_poll(&self, req: &mut Request, res: &mut Response) {
-
+    fn handle_poll(&self, req: &mut Request) -> Result<Vec<u8>> {
+        let poll_request: PollRequest = serde_json::from_reader(req)?;
+        Ok(serde_json::to_vec(&poll_request).unwrap())
     }
 }
 
@@ -43,26 +66,40 @@ impl hyper::server::Handler for EnclaveHandler {
         match req.uri {
             AbsolutePath(ref path) => match (&req.method, &path[..]) {
                 (&hyper::Post, "/poll") => {
-                    handle_poll(&mut req, &mut res);
+                    match self.handle_poll(&mut req) {
+                        Ok(response_contents) => {
+                            res.send(response_contents.as_slice()).unwrap();
+                        }
+                        Err(err) => {
+                            *res.status_mut() = hyper::BadRequest;
+                            res.send(format!("{:?}", err).as_bytes()).unwrap();
+                        }
+                    }
                 }
                 _ => {
                     *res.status_mut() = hyper::NotFound;
+                    res.start().unwrap();
                 }
             },
             _ => {
                 *res.status_mut() = hyper::NotFound;
+                res.start().unwrap();
             }
         }
-
-        let mut res = try_return!(res.start());
-        try_return!(copy(&mut req, &mut res));
     }
 }
 
-fn process_main(mut req: Request, mut res: Response) {
+fn main() {
+    setup_logging();
+    let connection = Connection::open_in_memory().unwrap();
+    let enclave_handler = EnclaveHandler::new(connection);
+    let _listening = hyper::Server::http("[::]:3000").unwrap().handle(enclave_handler);
+    println!("Listening on http://[::]:3000");
 }
 
-fn main() {
-    let _listening = hyper::Server::http("127.0.0.1:3000").unwrap().handle(process_main);
-    println!("Listening on http://127.0.0.1:3000");
+fn setup_logging() {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::Builder::from_default_env()
+        .target(env_logger::Target::Stdout)
+        .init();
 }
