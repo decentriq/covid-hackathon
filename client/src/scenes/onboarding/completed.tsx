9789eb407391b1b6ab06271d0d802b061304b7da
Enrico Ghirardi <i@choco.me>
2020-03-28 12:08:14 +0100

    add colors, style intro

---
+++ client/src/scenes/onboarding/completed.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableHighlight
+} from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { OnboardingNavigatorParamList } from "../../navigations/onboarding-navigator";
+import BackgroundGeolocation from "react-native-background-geolocation";
+
+type CompletedScreenNavigationProp = StackNavigationProp<
+  OnboardingNavigatorParamList,
+  'Completed'
+>;
+
+type CompletedProps = {
+  navigation: CompletedScreenNavigationProp;
+};
+
+export const CompletedScreen = ({navigation}: CompletedProps) => (
+  <SafeAreaView>
+    <Text>Screen: Completed</Text>
+
+    <TouchableHighlight onPress={
+        () => {
+          BackgroundGeolocation.requestPermission().then((_status) => {
+            navigation.navigate('Home')
+          }).catch((status) => {
+            console.log("REJECTED", status);
+          });
+        }
+    }>
+      <Text>Go to home</Text>
+    </TouchableHighlight>
+  </SafeAreaView>
+);
---
+++ client/src/scenes/onboarding/intro.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableHighlight,
+  Image,
+  StyleSheet
+} from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { OnboardingNavigatorParamList } from "../../navigations/onboarding-navigator";
+import { Colors } from "../../styles";
+
+type IntroScreenNavigationProp = StackNavigationProp<
+  OnboardingNavigatorParamList,
+  'Intro'
+>;
+
+type IntroProps = {
+  navigation: IntroScreenNavigationProp;
+};
+
+export const IntroScreen = ({navigation}: IntroProps) =>  {
+  navigation.setOptions({headerShown: false})
+  return (
+  <SafeAreaView style={styles.safeContainer}>
+    <View style={styles.container}>
+    <View style={styles.header}>
+      <Text style={styles.h1}>Cocotrace</Text>
+      <Text>Some more text explaining the app</Text>
+      <Text>Text that keeps goinText that keeps goinText that keeps goinText that keeps goinText that keeps goingggggText that keeps going</Text>
+    </View>
+    <View style={styles.info_section}>
+      <View style={styles.info_image_container}>
+        <Image style={styles.info_image} source={require("../../assets/images/safe.png")} />
+      </View>
+        <View style={styles.info_section_more}>
+          <Text style={styles.info_section_text}>How it works</Text>
+        </View>
+      </View>
+    <View style={styles.footer}>
+      <Text>Some more text</Text>
+      <TouchableHighlight underlayColor={Colors.DARK_ORANGE} style={styles.button} onPress={() => navigation.navigate('Completed')}>
+        <Text style={styles.buttonText}>
+          Sign Up
+        </Text>
+      </TouchableHighlight>
+    </View>
+    </View>
+  </SafeAreaView>
+)
+};
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white",
+  },
+  container: {
+    alignItems: "center",
+    backgroundColor: "white",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: 50,
+  },
+  header: {
+    flex: 2,
+    justifyContent: "flex-start",
+    alignSelf: "stretch",
+  },
+  h1: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  info_section: {
+    flex: 3,
+    justifyContent: "center",
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      width: 2,
+      height: 2
+    },
+    shadowRadius: 10,
+    borderRadius: 10,
+    margin: 30,
+  },
+  info_image: {
+    height: "100%",
+    aspectRatio: 1,
+    borderRadius: 10,
+  },
+  info_image_container: {
+    backgroundColor: "white",
+    height: "100%",
+    aspectRatio: 1,
+    borderRadius: 10,
+  },
+  info_section_more: {
+    backgroundColor: Colors.EXTRA_LIGHT_BLUE,
+    position: "absolute",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopColor: Colors.MAIN_BLUE,
+    borderTopWidth: 1,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  info_section_text: {
+    padding: 10,
+    fontWeight: "bold",
+    color: Colors.MAIN_BLUE,
+    fontSize: 30,
+  },
+  footer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: Colors.MAIN_ORANGE,
+    shadowColor: Colors.MAIN_ORANGE,
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      width: 2,
+      height: 2
+    },
+    shadowRadius: 10,
+    borderRadius: 10,
+    paddingLeft: 25,
+    paddingRight: 25
+  },
+  buttonText: {
+    padding: 5,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: Colors.LIGHT_ORANGE
+  }
+});
