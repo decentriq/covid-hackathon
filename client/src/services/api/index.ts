710582f47c351de33ec6bf96fd4bab292bb4f0f6
Enrico Ghirardi <i@choco.me>
2020-03-29 22:23:43 +0200

    Add client key to http header

--- client/src/services/api/index.ts
+++ client/src/services/api/index.ts
@@ -1,6 +1,47 @@
 import * as axios from 'axios';
 import { TextEncoder, TextDecoder } from 'text-encoding-shim';
 
+const base64abc = (() => {
+	let abc = [],
+		A = "A".charCodeAt(0),
+		a = "a".charCodeAt(0),
+		n = "0".charCodeAt(0);
+	for (let i = 0; i < 26; ++i) {
+		abc.push(String.fromCharCode(A + i));
+	}
+	for (let i = 0; i < 26; ++i) {
+		abc.push(String.fromCharCode(a + i));
+	}
+	for (let i = 0; i < 10; ++i) {
+		abc.push(String.fromCharCode(n + i));
+	}
+	abc.push("+");
+	abc.push("/");
+	return abc;
+})();
+
+function bytesToBase64(bytes: Uint8Array) {
+	let result = '', i, l = bytes.length;
+	for (i = 2; i < l; i += 3) {
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+		result += base64abc[((bytes[i - 1] & 0x0F) << 2) | (bytes[i] >> 6)];
+		result += base64abc[bytes[i] & 0x3F];
+	}
+	if (i === l + 1) { // 1 octet missing
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[(bytes[i - 2] & 0x03) << 4];
+		result += "==";
+	}
+	if (i === l) { // 2 octets missing
+		result += base64abc[bytes[i - 2] >> 2];
+		result += base64abc[((bytes[i - 2] & 0x03) << 4) | (bytes[i - 1] >> 4)];
+		result += base64abc[(bytes[i - 1] & 0x0F) << 2];
+		result += "=";
+	}
+	return result;
+}
+
 interface Illness {
     start_time: Date,
     duration_days?: number 
@@ -71,9 +112,9 @@ function decrypt (
 export class API {
   session: axios.AxiosInstance;
 
-  constructor(authorizationToken: string) {
+  constructor(clientKey: Uint8Array) {
     this.session = axios.default.create({
-      headers: {Authorization: `Bearer ${authorizationToken}`},
+      headers: {ClientPubKey: bytesToBase64(clientKey)},
     });
   }
 
