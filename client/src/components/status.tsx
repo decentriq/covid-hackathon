eeee64e296f6c49add9ab1a942716656f4ceb832
Stefan Deml <stefandeml@gmail.com>
2020-03-30 14:41:18 +0200

    wip

---
+++ client/src/components/status.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import {Text, Button, View, ScrollView} from 'react-native';
+import {connect, ConnectedProps} from 'react-redux';
+import {RootState} from '../store';
+import {CurrentStatus} from '../store/general/types';
+import {changeStatus, changeIllness} from '../store/general/actions';
+import {Colors} from '../styles';
+
+type MyProps = {};
+type Props = PropsFromRedux & MyProps;
+
+type State = {};
+
+class StatusComponent extends React.Component<Props, State> {
+  state: State = {};
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  triggerInfected() {
+    this.props.changeStatus(CurrentStatus.Infected);
+    this.props.changeIllness({
+      start_time: new Date(),
+      duration_days: 14,
+    });
+  }
+
+  triggerUpdate() {
+    // here we need to do the following:
+    // * convert location to TimeStampCoordinates
+    // * send request to the
+    // * if exposed is set then update exposed => call this funtion
+  }
+
+  getStatusRender(status: CurrentStatus) {
+    switch (status) {
+      case CurrentStatus.Recording:
+        return (
+          <View style={{backgroundColor: Colors.EXTRA_EXTRA_LIGHT_BLUE}}>
+            <Text style={{color: Colors.MAIN_BLUE, fontSize: 25}}>
+              Recording
+            </Text>
+          </View>
+        );
+      case CurrentStatus.Infected:
+        return (
+          <View style={{backgroundColor: Colors.LIGHT_ORANGE}}>
+            <Text style={{color: Colors.MAIN_ORANGE, fontSize: 25}}>
+              Infected
+            </Text>
+          </View>
+        );
+      case CurrentStatus.Exposed:
+        return (
+          <View style={{backgroundColor: Colors.DARK_ORANGE}}>
+            <Text style={{color: Colors.MAIN_BLUE, fontSize: 25}}>Exposed</Text>
+          </View>
+        );
+      default:
+        throw Error('Unknown Status');
+    }
+  }
+  render() {
+    const {locations, enclave_identity, status} = this.props;
+    const last_location = locations.slice(-1)[0];
+    const status_render = this.getStatusRender(status);
+
+    return (
+      <ScrollView>
+        <View
+          style={{flex: 1, justifyContent: 'center', alignItems: 'flex-start'}}>
+          <Text>Last Location Data: {'\n' + last_location.timestamp}</Text>
+
+          {status_render}
+
+          <Button
+            onPress={() => this.triggerInfected()}
+            title="Diagnosed and Infected"
+          />
+
+          <Text>Enclave Identity: {'\n' + enclave_identity}</Text>
+        </View>
+
+        <Button onPress={() => this.triggerUpdate()} title="Update" />
+      </ScrollView>
+    );
+  }
+}
+
+const mapState = (state: RootState) => ({
+  locations: state.traces.locations,
+  enclave_identity: state.general.enclave_identity,
+  status: state.general.current_status,
+});
+
+const mapDispatch = {
+  changeStatus: changeStatus,
+  changeIllness: changeIllness,
+};
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+const connector = connect(mapState, mapDispatch);
+export default connector(StatusComponent);
---
+++ client/src/store/general/types.ts
@@ -0,0 +1,35 @@
+import {Illness as BaseIllness} from '../../services/api';
+export interface Illness extends BaseIllness {}
+
+export type GeneralStore = {
+  enclave_identity: any;
+  current_status: CurrentStatus;
+  illness: Illness | null;
+};
+
+export enum CurrentStatus {
+  Recording,
+  Exposed,
+  Infected,
+}
+
+export const CHANGE_STATUS = 'CHANGE_STATUS';
+export const CHANGE_ENCLAVE_IDENT = 'CHANGE_ENCLAVE_IDENT';
+export const CHANGE_ILLNESS = 'CHANGE_ILLNESS';
+
+interface ChangeStatus {
+  type: typeof CHANGE_STATUS;
+  new_status: CurrentStatus;
+}
+
+interface ChangeReport {
+  type: typeof CHANGE_ENCLAVE_IDENT;
+  new_ident: any;
+}
+
+interface ChangeIllness {
+  type: typeof CHANGE_ILLNESS;
+  illness: Illness;
+}
+
+export type GeneralActionTypes = ChangeStatus | ChangeReport | ChangeIllness;
